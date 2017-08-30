9b75a06b1f6989ee3d52f91cb5c47db486d27185
csgajcr <610691723@qq.com>
2017-08-30 15:10:09 +0800

    添加 搜索功能

--- application/app/controller/Member.php
+++ application/app/controller/Member.php
@@ -3,9 +3,12 @@
 namespace app\app\controller;
 
 use app\app\model\MemberDO;
+use think\Cookie;
 
 class Member extends BaseController
 {
+    private static $ADMIN_FILE_PATH = APP_PATH."AdminUserId.txt";
+    private static $COOKIE_KEY_NAME = 'wQ4c_e043_lastcheckfeed';
 
     public function index()
     {
@@ -26,18 +29,40 @@ class Member extends BaseController
             return $this->buildErrorResult('pageSize need 1-100');
         }
         $this->recordLog('getAll() methods called requestPage=' . $requestPage . ' pageSize=' . $pageSize);
-        return $this->buildMemberListResult($requestPage, $pageSize);
+        return $this->buildSuccessResult(MemberDO::queryByPagination($requestPage, $pageSize));
     }
 
-    private function buildMemberListResult($requestPage, $pageSize)
+    public function search(){
+        $keyword = input('keyword');
+        if ($keyword==null){
+            return $this->buildErrorResult('keyword can not be null');
+        }
+        $this->recordLog('search() methods called searchKeyword='.$keyword);
+        return $this->buildSuccessResult(MemberDO::searchMember($keyword));
+    }
+
+    private function getDiscuzLoggedUser()
     {
-        $member = new MemberDO();
-        $totalCount = $member->count();
-        $dataList = $member->limit(($requestPage - 1) * $pageSize, $pageSize)->select();
-        return $this->buildSuccessResult([
-            'totalCount' => $totalCount,
-            'data' => $dataList
-        ]);
-
+        $bbsCookie = Cookie::get(self::$COOKIE_KEY_NAME);
+        if ($bbsCookie != null) {
+            $userId = explode("|", $bbsCookie)[0];
+            return $userId;
+        } else {
+            return "";
+        }
     }
+
+    private function validateUserPrivilege($userId)
+    {
+        if (!file_exists(self::$ADMIN_FILE_PATH)) {
+            return false;
+        }
+        $fp = fopen(self::$ADMIN_FILE_PATH,"r");
+        $content = fread($fp, filesize(self::$ADMIN_FILE_PATH));
+        $adminList = explode(",", $content);
+        fclose($fp);
+        return in_array($userId,$adminList);
+    }
+
+
 }
--- application/app/model/MemberDO.php
+++ application/app/model/MemberDO.php
@@ -9,9 +9,41 @@
 namespace app\app\model;
 
 
+use think\Db;
 use think\Model;
 
 class MemberDO extends Model
 {
+    protected static $tableName = 'member';
     protected $table = 'member';
+
+    public static function queryByPagination($requestPage, $pageSize)
+    {
+        $member = new MemberDO();
+        $totalCount = $member->count();
+        if ($totalCount - $requestPage * $pageSize < 0) {
+            $offset = 0;
+            $pageSize = $totalCount - ($requestPage - 1) * $pageSize;
+        } else {
+            $offset = $totalCount - $requestPage * $pageSize;
+        }
+        $subQuery = Db::table(self::$tableName)->limit($offset, $pageSize)->buildSql(false);
+        $dataList = Db::table('(' . $subQuery . ') a')
+            ->order('id', 'desc')
+            ->select();
+        return [
+            'totalCount' => $totalCount,
+            'data' => $dataList
+        ];
+    }
+
+    public static function searchMember($keyword)
+    {
+        $dataList = Db::table(self::$tableName)->where('name', 'like', '%' . $keyword . '%')
+            ->limit(10)->select();
+        return [
+            'totalCount' => count($dataList),
+            'data' => $dataList
+        ];
+    }
 }
